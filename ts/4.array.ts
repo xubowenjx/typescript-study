1145bb7e17449bec58b0b9479d6eedd52550897e
xubowenjx <286129936@qq.com>
2017-11-16 23:07:24 +0800

    add array & vscode setting

---
+++ ts/4.array.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: xubowen 
+ * @Date: 2017-11-16 22:48:10 
+ * @Last Modified by: xubowen
+ * @Last Modified time: 2017-11-16 23:03:52
+ */
+let num_arr:number[] = [1,2,3,4,5];
+let str_arr:string[] =['1','2'];
+let any_arr:any[] = [1,'2','3',{}];
+let arr:Array<number> = [1,2,3,4];
+console.log(arr);
